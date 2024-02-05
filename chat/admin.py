48f71262915adc6da81ee0198df80ecc917b198c
sinasezza <s.e.sezza121@gmail.com>
2024-02-05 23:33:01 +0330

    fix(chat): bugs fixed.

--- chat/admin.py
+++ chat/admin.py
@@ -4,6 +4,7 @@ from .models import Message, Thread
 class MessageInline(admin.TabularInline):  # You can also use StackedInline
     model = Message
     extra = 0  # Set to 0 to avoid showing empty form fields
+    ordering = ('created_at',)
 
 @admin.register(Thread)
 class ThreadAdmin(admin.ModelAdmin):
--- chat/consumers.py
+++ chat/consumers.py
@@ -1,13 +1,14 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
+from django.db.models import Q
+from accounts.models import Account
 from . import models
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f"chat_{self.room_name}"
-        print(f"\n\n\n Connecting to room: {self.room_name} \n\n\n")
 
         # Join room group
         await self.channel_layer.group_add(
@@ -27,11 +28,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        sender = text_data_json['sender']
-        receiver = text_data_json['receiver']
+        sender_username = text_data_json['sender']
+        receiver_username = text_data_json['receiver']
 
         # Save message to the database
-        await self.save_message(sender, receiver, message)
+        await self.save_message(sender_username, receiver_username, message)
 
         # Send message to room group
         await self.channel_layer.group_send(
@@ -51,20 +52,19 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'message': message,
         }))
 
-    async def save_message(self, sender, receiver, message_content):
+    async def save_message(self, sender_username, receiver_username, message_content):
         # Retrieve users and thread based on your application logic
         # user1 = self.scope['user']
-        user1 = sender
-        
-        print(f"\n\n\nuser1 is {user1}\n\n\n")
-        user2 = receiver  # Set the second user based on your application logic
-        thread = None  # Set the thread based on your application logic
+        sender = await sync_to_async(Account.objects.get)(username=sender_username)
+        receiver = await sync_to_async(Account.objects.get)(username=receiver_username)
+
+        thread = await sync_to_async(models.Thread.objects.get)((Q(user1=sender) &  Q(user2=receiver)) | (Q(user1=receiver) & Q(user2=sender)))
 
         # Save the message to the database
         await sync_to_async(models.Message.objects.create)(
             thread=thread,
-            from_user=user1,
-            to_user=user2,
+            from_user=sender,
+            to_user=receiver,
             content=message_content
         )
 
