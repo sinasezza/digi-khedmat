4aa7d61ccc396aa479132be853a1bcb35d8ec7b7
sinasezza <s.e.sezza121@gmail.com>
2024-02-22 23:35:58 +0330

    ref(barters, accounts): views and templates refactored.

--- barters/admin.py
+++ barters/admin.py
@@ -1,25 +1,49 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from import_export import resources
 from import_export.admin import ImportExportActionModelAdmin
 from . import models
 
 
+class BarterImageInline(admin.StackedInline):
+    model = models.BarterImage
+    extra = 1
+    ordering = ('-id',)
+    readonly_fields = ('image_tag',)
+
+    def image_tag(self, obj):
+        if obj.image:
+            return mark_safe('<img src="{0}" style="max-width:200px;max-height:200px" />'.format(obj.image.url))
+        else:
+            return '-'
+
+    image_tag.short_description = 'Image Preview'
 
 class BarterResource(resources.ModelResource):
     class Meta:
         model = models.BarterAdvertising
         
 @admin.register(models.BarterAdvertising)
-class BarterAdvertisingAdmin(admin.ModelAdmin):
+class BarterAdvertisingAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
     list_display = ('id','title', 'date_created', 'status',)
     ordering = ['-date_created',]
     
+    inlines = [BarterImageInline,]
     resource_classes = [BarterResource,]
 
 # ================================================
     
 @admin.register(models.BarterImage)
 class BarterImageAdmin(admin.ModelAdmin):
-    list_display = ('title','image', 'barter_advertising')
+    list_display = ('title', 'image', 'barter_advertising')
+    readonly_fields = ('image_tag',)
+
+    def image_tag(self, obj):
+        if obj.image:
+            return mark_safe('<img src="{0}" style="max-width:200px;max-height:200px" />'.format(obj.image.url))
+        else:
+            return '-'
+
+    image_tag.short_description = 'Image Preview'
 
 # ================================================
